55bc2951d7fbf18194b0fb3ee3da6b4c9a86c66d
Ubuntu <ds@live.com>
2013-07-11 11:31:59 +0000

    Done

--- web.js
+++ web.js
@@ -1,11 +1,13 @@
 var express = require('express');
+var fs=require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs.readFileSync('index.html');
+  var str=fs.toString();
+  response.send(str);
 });
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
