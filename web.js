ec8d7d9f0b7da0682211ec1796cf03063e2958db
dscarson <dscarson@live.com>
2013-07-24 17:46:25 +0000

    done

--- web.js
+++ web.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var fs=require('fs');
-
+var htmlfile="index.html";
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buf = new Buffer(250);
-
+ // var buf = new Buffer(250);
+  var html= fs.readFileSync(htmlfile).toString();
   buf=fs.readFileSync('index.html');
-  var str=buf.toString();
+ //  var str=buf.toString();
   response.send(str);
 });
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
